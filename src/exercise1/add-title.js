883151c9e2e66710728a7b5ad746a40af50f283d
Jordan Hall <jordan@libertyware.co.uk>
2022-06-07 11:33:30 +0100

    Add comment typing
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- src/exercise1/add-title.js
+++ src/exercise1/add-title.js
@@ -1,3 +1,9 @@
+// Make me into a typescript file and Assign from app param to HTMLElement
+
+/**
+ * @param {HTMLElement} app
+ */
+
 export function appendTitle(app) {
 	app.innerHTML = `<h1>Welcome to an introduction to JavScript and Typescript!</h1>`
 }
