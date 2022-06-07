13ff92c77e936189abf3cf4b25856178295ae9d4
Jordan Hall <jordan@libertyware.co.uk>
2022-06-07 11:18:33 +0100

    fix: repair spelling
    Signed-off-by: Jordan Hall <jordan@libertyware.co.uk>

--- src/exercise1/add-title.spec.js
+++ src/exercise1/add-title.spec.js
@@ -15,13 +15,13 @@ test('Check if the JS is working', () => {
 	expect(h1.textContent).toBe('Welcome to an introduction to JavScript and Typescript!')
 })
 
-test('Calling appendTitle will through an error', () => {
+test('Calling appendTitle will throw an error', () => {
 	const addToMe = {};
 	appendTitle(addTome);
 	const h1 = element.querySelector('h1');
 
 	expect(h1).not.toBeNull();
-	expect(h1.textContent).toBe('Welcome to an introduction to JavScript and Typescript!')
+	expect(h1.textContent).toBe('Welcome to an introduction to JavaScript and Typescript!')
 })
 
 test('Wrong type but passes', () => {
